9a307b7d2b984c1e64653006dd2497aacf30ad4d
Jorge Devia <it@mabecenter.org>
2024-12-19 14:05:47 -0500

    ok

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    argocd = {
+      source = "argoproj-labs/argocd"
+      version = "7.2.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "argocd" {
   port_forward_with_namespace = "argocd"
   insecure    = true
