437b6938c1d9f926cf728761660bd7831b786115
Jorge Devia <it@mabecenter.org>
2024-12-19 14:18:43 -0500

    ok

--- providers.tf
+++ providers.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     argocd = {
-      source = "argoproj-labs/argocd"
-      version = "7.2.0"
+      source  = "argoproj-labs/argocd"
+      version = "7.2.0"  # Asegúrate de que esta versión coincide con la que defines en el root
     }
   }
+}
 
-  required_version = ">= 1.2.0"
+provider "argocd" {
+  # La configuración se hereda del módulo principal
 }
