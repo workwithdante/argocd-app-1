2812d07db2525ef608c79e7e4d3f2fdaee8be775
Jorge Devia <it@mabecenter.org>
2024-12-19 14:48:08 -0500

    ok

--- main.tf
+++ main.tf
@@ -1,3 +1,34 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    argocd = {
+      source = "argoproj-labs/argocd"
+      version = "7.2.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "kubernetes" {
+  # Se hereda del módulo principal
+}
+
+provider "helm" {
+  # Se hereda del módulo principal
+}
+
+provider "argocd" {
+  # Se hereda del módulo principal
+}
+
 resource "kubernetes_namespace" "nfs" {
   metadata {
     name = "nfs"
