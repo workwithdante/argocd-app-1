1853a6c9865e2f09bb47fb9fe159c0c305fc5878
Jorge Devia <it@mabecenter.org>
2024-12-19 16:55:13 -0500

    ok

--- main.tf
+++ main.tf
@@ -153,6 +153,7 @@ resource "kubernetes_manifest" "argocd_application_erpnext" {
     kubernetes_namespace.erpnext
   ]
 
-  manifest = yamldecode(file("./terraform/modules/argocd/frappe/mabecenter.yaml"))
-
+  manifest = yamldecode(templatefile("${path.module}/modules/argocd/frappe/mabecenter.yaml", {
+    branch_name = var.branch_name
+  }))
 }
