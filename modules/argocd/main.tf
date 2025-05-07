742474b59f7202f64dce9c84e430e51aaff003e5
Jorge Devia <it@mabecenter.org>
2025-05-07 16:02:16 -0500

    ok

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -4,14 +4,7 @@ resource "kubernetes_namespace" "argocd" {
   }
 }
 
-# Instalar los CRDs de ArgoCD aplicando directamente el YAML oficial
-resource "kubernetes_manifest" "argocd_crds" {
-  manifest = yamldecode(file("${path.module}/crds.yaml"))
-}
-
-# Helm release principal de ArgoCD, que depende de los CRDs
 resource "helm_release" "argocd" {
-  depends_on       = [ kubernetes_manifest.argocd_crds ]
   name             = var.config.release_name
   repository       = var.config.repository
   chart            = var.config.chart
